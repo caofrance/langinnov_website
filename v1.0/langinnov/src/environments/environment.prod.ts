e82c4cd9046cf722da5d60b02aba01cc511ef86e
Kris <madden_kris@yahoo.com>
2019-07-16 20:24:36 +0200

    removing orginal website

--- v1.0/langinnov/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  server_url: 'https://coml.lscp.ens.fr/bigbaby',
-  production: true,
-  under_construction: true,
-  /* congrats to the person who found this you are L33t H4ckZ0R */
-  key: '123password',
-};
